b4c9ef78bdbee0c62d9272ef84c06a8d38fc2257
cevenaris <cyzsidelt@gmail.com>
2021-09-03 22:45:21 -0700

    Reorganized the demos folder, moved demos that don't work to tests and gave credit where necessary

---
+++ src/misc/demoCredits.gd
@@ -0,0 +1,6 @@
+extends Reference
+
+# made this a script so that it would show up in the filesystem dock
+
+# Godot character model: "GodotV2 WIP" (https://skfb.ly/ooWSJ) by ImageParSeconde is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
+# botw toon shader: BoTW Toon Shader by NekotoArts
